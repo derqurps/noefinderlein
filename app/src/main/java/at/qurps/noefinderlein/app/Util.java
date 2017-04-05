73b452dd94be107346ad60760f86215af7f02082
Roman Reitschmied <derqurps@gmail.com>
2017-04-05 00:13:28 -0400

    add some basic game things

--- app/src/main/java/at/qurps/noefinderlein/app/Util.java
+++ app/src/main/java/at/qurps/noefinderlein/app/Util.java
@@ -410,17 +410,28 @@ public class Util {
 
         editor.apply();
     }
-    public final static int getDatePreferencesYear(Context mContext) {
+    public final static void setPreferencesBoolean(Context mContext, String key, boolean setVar) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-        return preferences.getInt(DialogFragment_ChooseDates.YEAR, 0);
+        SharedPreferences.Editor mEditor =preferences.edit();
+        mEditor.putBoolean(key, setVar);
+        mEditor.apply();
+    }
+    public final static boolean getPreferencesBoolean(Context mContext, String key, boolean defaultVar) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        return preferences.getBoolean(key, defaultVar);
+    }
+    public final static int getPreferencesInt(Context mContext, String key, int defaultVar) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+        return preferences.getInt(key, defaultVar);
+    }
+    public final static int getDatePreferencesYear(Context mContext) {
+        return getPreferencesInt(mContext, DialogFragment_ChooseDates.YEAR, 0);
     }
     public final static int getDatePreferencesMonth(Context mContext) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-        return preferences.getInt(DialogFragment_ChooseDates.MONTH, 0);
+        return getPreferencesInt(mContext, DialogFragment_ChooseDates.MONTH, 0);
     }
     public final static int getDatePreferencesDay(Context mContext) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-        return preferences.getInt(DialogFragment_ChooseDates.DAY, 0);
+        return getPreferencesInt(mContext, DialogFragment_ChooseDates.DAY, 0);
     }
     private static Calendar getChosenDayAsCalendar(Context mContext){
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext);
@@ -462,41 +473,5 @@ public class Util {
         int month = preferences.getInt(DialogFragment_ChooseDates.MONTH, 0);
         int day = preferences.getInt(DialogFragment_ChooseDates.DAY, 0);
         return ((year == (int)cNow.get(Calendar.YEAR)) && (month == (int)cNow.get(Calendar.MONTH)) && (day == (int)cNow.get(Calendar.DAY_OF_MONTH)));
-    }
-    public static Dialog makeSimpleDialog(Activity activity, String text) {
-        return (new AlertDialog.Builder(activity)).setMessage(text)
-                .setNeutralButton(android.R.string.ok, null).create();
-    }
-    public static boolean resolveConnectionFailure(Activity activity,
-                                                   GoogleApiClient client, ConnectionResult result, int requestCode,
-                                                   String fallbackErrorMessage) {
-
-        if (result.hasResolution()) {
-            try {
-                result.startResolutionForResult(activity, requestCode);
-                return true;
-            } catch (IntentSender.SendIntentException e) {
-                // The intent was canceled before it was sent.  Return to the default
-                // state and attempt to connect to get an updated ConnectionResult.
-                client.connect();
-                return false;
-            }
-        } else {
-            // not resolvable... so show an error message
-            int errorCode = result.getErrorCode();
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(errorCode,
-                    activity, requestCode);
-            if (dialog != null) {
-                dialog.show();
-            } else {
-                // no built-in dialog: show the fallback error message
-                showAlert(activity, fallbackErrorMessage);
-            }
-            return false;
-        }
-    }
-    public static void showAlert(Activity activity, String message) {
-        (new AlertDialog.Builder(activity)).setMessage(message)
-                .setNeutralButton(android.R.string.ok, null).create().show();
     }
 }
